b73edc15cebd04e0fd1539993117cf9a2663064c
shubhanshuyadav <shubhanshuemailid@gmail.com>
2021-05-04 00:19:45 +0530

    createArea for notes creating is done

--- keeper_app/src/component/App.jsx
+++ keeper_app/src/component/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import CreateArea from "./CreateArea";
 import Header from "./Header"
 
 function App(){
   return(
     <div>
       <Header/>
+      <CreateArea/>
     </div>
   );
 }
---
+++ keeper_app/src/component/CreateArea.jsx
@@ -0,0 +1,19 @@
+function CreateArea(){
+    return(
+            <form action=""> <input 
+                type="text"
+                placeholder="Title"
+            />
+            <textarea 
+                rows="3"
+                placeholder="Take note..."
+            
+            ></textarea>
+
+            <button>Add</button>
+            </form>  
+    
+    );
+}
+
+export default CreateArea;
