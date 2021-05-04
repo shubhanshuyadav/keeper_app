cebc06eab2ad07a63c0463f6bdefdedda376caa5
shubhanshuyadav <shubhanshuemailid@gmail.com>
2021-05-04 13:59:25 +0530

    animation added

--- keeper_app/src/component/CreateArea.jsx
+++ keeper_app/src/component/CreateArea.jsx
@@ -1,10 +1,20 @@
 import {useState} from "react"
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom'
 
 function CreateArea(props){
 const [note,setNote] = useState({
     title:"",
     content:""
 });
+
+const[isExpanded, setIsExpanded]=useState(false);
+
+
+function handleIsExpand(){
+    setIsExpanded(true);
+}
 
 function handleChange(event){
     const {name, value}=event.target;
@@ -25,25 +35,29 @@ function submitNote(event){
 }
 
     return(
-            <form> 
-            <input 
+            <form className="create-area"> 
+           
+            {isExpanded && <input 
                 type="text"
                 name="title"
                 placeholder="Title"
                 value={note.title}
                 onChange={handleChange}
-                />
+                />}
 
             <textarea 
                 type="text"
                 name="content"
                 value={note.content}
                 onChange={handleChange}
-                rows="3"
+                rows={isExpanded?3:1}
+                onClick={handleIsExpand}
                 placeholder="Take note...">
             </textarea>
 
-            <button onClick={submitNote}>Add</button>
+            <Zoom in={isExpanded}><Fab onClick={submitNote}><AddIcon/></Fab></Zoom>
+            
+            
             </form>  
     
     );
